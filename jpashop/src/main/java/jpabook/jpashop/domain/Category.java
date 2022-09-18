1f1a1f21452868a12f907dfbb1072964165560bd
sdfgx123 <sdfgx123@naver.com>
2022-09-18 19:10:04 +0900

    20220918

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Category {
+
+    @Id @GeneratedValue
+    @Column(name = "categor y_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "category_item",
+        joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id"))
+    private List<Item> items = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    // 셀프로 연관관계를 걸었다
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+
+}
